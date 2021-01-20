b46bd8e811f1f11369cc86f266a8d7ede2acbb59
Elena Chechulina <elenachechulina@Elenas-MacBook-Pro.local>
2021-01-20 16:01:31 -0500

    fixing Express

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express'); // like a PHP require
+const path = require('path');
 
 const server = express();
-const path = require('path');
+
 
 // set our views directory
 server.set("views", path.join(__dirname, 'views'));
@@ -11,12 +12,12 @@ server.use(express.static(path.join(__dirname, "public")));
 
 server.get("/", (req, res) => {
   console.log('hit the home route');
-  res.sendFile('views/index.html');
+  res.sendFile(path.join(__dirname, 'views/index.html')); 
 })
 
 server.get("/contact", (req, res) => {
   console.log('hit the contact route');
-  res.sendFile('views/contact.html');
+  res.sendFile(path.join(__dirname, 'views/contact.html'));
 })
 
 const port = process.env.PORT || 3000; // localhost:3000
